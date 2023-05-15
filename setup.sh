7a7a82454f77d449357a8a66a138102ff0af54d4
sherlock-ohm <100420979+sherlock-ohm@users.noreply.github.com>
2023-05-15 13:00:06 +1000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,62 @@
+#!/bin/zsh
+sudo apt update
+sudo apt install golang -y
+sudo apt install testssl.sh -y
+sudo apt install cloud-enum -y
+go install github.com/harleo/knockknock@latest
+go install github.com/sensepost/gowitness@latest
+go install -v github.com/s0md3v/smap/cmd/smap@latest
+go install -v github.com/hakluke/haktrails@latest
+echo "alias haktrails=~/go/bin/haktrails" >> ~/.zshrc
+mkdir ~/.config/haktools
+echo "securitytrails:\n  key: <insert API Key here>" > ~/.config/haktools/haktrails-config.yml
+echo 'apikeys:
+  bevigil:
+   key:
+
+  binaryedge:
+    key:
+
+  bing:
+    key:
+
+  censys:
+    id:
+    secret:
+
+  fullhunt:
+    key:
+
+  github:
+    key:
+
+  hunter:
+    key:
+
+  intelx:
+    key:
+
+  pentestTools:
+    key:
+
+  projectDiscovery:
+    key:
+
+  rocketreach:
+    key:
+
+  securityTrails:
+    key:
+
+  shodan:
+    key:
+
+  virustotal:
+    key:
+
+  zoomeye:
+    key:' | sudo tee /etc/theHarvester/api-keys.yaml    
+sudo chmod +x osinty
+sudo cp osinty /opt
+sudo chown kali:kali /opt/osinty
+echo "export PATH=$PATH:/opt >> ~/.zshrc"
